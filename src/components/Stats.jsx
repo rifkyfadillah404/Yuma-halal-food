0f9d2a869cf8015ab1b214589710aed4e60413b7
rifkyfadillah404 <rifkyfadillah404@gmail.com>
2025-11-13 11:55:26 +0700

    5+

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -4,7 +4,7 @@ import { IoCalendar, IoPeople, IoGrid, IoCheckmarkCircle } from 'react-icons/io5
 const Stats = () => {
   const stats = [
     { 
-      number: '2+', 
+      number: '5+', 
       label: 'Tahun di Tokyo',
       icon: IoCalendar,
       color: 'from-blue-500 to-blue-600'
